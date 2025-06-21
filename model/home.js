bcc9ee6d3cf6f00858f15e11b73183fb90a47195
Jenish Rabdiya <jenisharabadiya277@gmail.com>
2025-06-21 15:13:32 +0530

    model

---
+++ model/home.js
@@ -0,0 +1,51 @@
+const { Schema, model, Types } = require("mongoose");
+
+const collectionSchema = new Schema(
+    {
+        slider: {
+            type: Array,
+            default: [],
+        },
+        sections: [
+            {
+                img: {
+                    type: String,
+                    default: null,
+                },
+                mainTitle: {
+                    type: String,
+                    default: null,
+                },
+                title: {
+                    type: String,
+                    default: null,
+                },
+                discription: {
+                    type: String,
+                    default: null,
+                },
+                link: {
+                    type: String,
+                    default: null,
+                },
+            }
+        ],
+        followOn: [
+            {
+                img: {
+                    type: String,
+                    default: null,
+                },
+                link: {
+                    type: String,
+                    default: null,
+                },
+            }
+        ]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = model("home", collectionSchema);
